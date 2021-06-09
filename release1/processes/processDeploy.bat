66223d3fd9e382a2a97ffa75f474042b01e75376
Emerson - Avanade <Jonas.da.silva@avanade.com>
2021-06-09 15:34:33 -0300

    OK

---
+++ release1/processes/processDeploy.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease 
+EXEC01.bprelease /overwrite /user %1 %2 /dbconname %3
---
+++ release1/releases/releasedeploy.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease 
+EXEC01.bprelease /overwrite /user %1 %2 /dbconname %3
