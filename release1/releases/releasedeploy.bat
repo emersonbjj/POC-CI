de91a2d057b9ea40b3de63004f76683de8af51e2
Emerson - Avanade <Jonas.da.silva@avanade.com>
2021-06-09 15:51:44 -0300

    Testin BAT Release

--- release1/releases/releasedeploy.bat
+++ release1/releases/releasedeploy.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate
-start .\AutomateC.exe" /importrelease EXEC01.bprelease /overwrite /user %1 %2 /dbconname %3
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease 
+EXEC01.bprelease /overwrite /user %1 %2 /dbconname %3
