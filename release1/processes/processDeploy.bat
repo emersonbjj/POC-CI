37d8d1e70358868f628b8b23ef19b913b6c7fe6a
Emerson Ferreira <33907714+emersonbjj@users.noreply.github.com>
2021-06-09 17:43:33 -0300

    Update processDeploy.bat

--- release1/processes/processDeploy.bat
+++ release1/processes/processDeploy.bat
@@ -1,2 +1,1 @@
-cd C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease 
-EXEC01.bprelease /overwrite /user %1 %2 /dbconname %3
+cd C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease EXEC01.bprelease /overwrite /user %1 %2 /dbconname %3
